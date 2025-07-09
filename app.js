026965781848dcfcfd2d5f6ee6030d16ca4ebea5
Mohit Verma <156345473+Mohit12345789@users.noreply.github.com>
2025-07-09 20:25:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("✅ Node.js app deployed to AWS Elastic Beanstalk!");
+  res.send(" Node.js app deployed to AWS Elastic Beanstalk! & Hey i am Mohit");
 });
 
 app.listen(port, () => {
